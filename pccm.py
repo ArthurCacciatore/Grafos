fad058e802ca62174664ae022c47a1e178e69ec3
Arthur Cacciatore <arthurcacciatori@gmail.com>
2025-06-14 18:45:02 -0300

    feat: Implementa Belman-Ford Ordenado

--- pccm.py
+++ pccm.py
@@ -21,7 +21,7 @@ def ler_grafo(caminho_arquivo):
 
 
 
-def ordem(n, s, valor ): #se valor = 0 par se = 1 impar
+def ordem_atual(n, s, valor ): #se valor = 0 par se = 1 impar   (perguntar para o vagner se usar essa funcao afetaria o desempenho para grafos GIGANTES)
     if valor == 0:
         ordem = list(range(n))
         ordem.remove(s)
@@ -33,16 +33,42 @@ def ordem(n, s, valor ): #se valor = 0 par se = 1 impar
     return ordem
 
 
+def PCCM(grafo,n, s, ant , dist): #perguntar para o wagner se aqui pode ter o n, pois no exemplo nao tem (?o programa funcionava sem passar o n?)
+    dist = [INFINITO] * n
+    ant = [-1] * n
+    dist[s] = 0
+    ordem_impar = ordem_atual(n,s,0)
+    ordem_par = ordem_atual (n,s,1)
+    print("O I", ordem_impar)
+    print("O P", ordem_par)
 
+    k=1
+    atualiza = True
+    while(k <= n - 1  and atualiza):
+        atualiza = False
+        ordem = ordem_impar if  k % 2 ==1 else ordem_par
+        for u in ordem:
+            for d,c in grafo[u]:
+                if dist[u] + c < dist[d]:
+                    dist[d] = dist[u] + c
+                    ant[d] = u
+                    atualiza = True
+        if not atualiza:
+            break
+        k+= 1
+    print(k)
+    print(dist)
+    print(ant)
+    if k == n:
+        print("existe ciclo negativo")
+                 
+    
 
 caminho_Arquivo = sys.argv[1];
 s = int(sys.argv[2]);
 grafo, n = ler_grafo("Arquivos/" + caminho_Arquivo)
-ordem_impar = ordem(n,s,0)
-ordem_par = ordem (n,s,1)
-print("O I", ordem_impar)
-print("O P", ordem_par)
-distancia = [INFINITO] * n
-distancia[s] = 0
-anterior = [-1] * n
+distancia = []
+anterior = []
+PCCM(grafo,n, s, anterior, distancia)
+
 
